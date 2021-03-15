54351dba5e02abc935cd3b91feefe3a81d8cc772
Nicholas Anderson <nickanderson1998@gmail.com>
2021-03-14 22:34:59 -0400

    March 2021 Updates

---
+++ user/plugins/oidc/vendor/composer/installed.php
@@ -0,0 +1,53 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9a1d9a00f5a12654a9ecab037d488121182c52cf',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9a1d9a00f5a12654a9ecab037d488121182c52cf',
+    ),
+    'jumbojett/openid-connect-php' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '4f95102af87f86c43e8191ec6e90d9f35ed1ce5f',
+    ),
+    'paragonie/random_compat' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '446fc9faa5c2a9ddf65eb7121c0af7e857295241',
+    ),
+    'phpseclib/phpseclib' => 
+    array (
+      'pretty_version' => '2.0.x-dev',
+      'version' => '2.0.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cf8d62500bd869eca939553918e50f35baffd413',
+    ),
+  ),
+);
